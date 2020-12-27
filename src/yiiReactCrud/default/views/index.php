61bf6d07366e59ae9caaf8a2024ecaaa7c05e8fb
George <tomiuc117@gmail.com>
2020-12-27 11:46:24 +0200

    fixed missing imports and typos

--- src/yiiReactCrud/default/views/index.php
+++ src/yiiReactCrud/default/views/index.php
@@ -16,20 +16,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Link, Redirect} from "react-router-dom";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import {setBreadcrumbs} from "../../redux/actions";
-import {DataView, PrepareIndexModal, FetchIndexData, ManipulateIndexData} from "react-yii2-essentials";
+import {DataView, PrepareIndexModal, FetchIndexData, ManipulateIndexData, IndexDataLoader} from "react-yii2-essentials";
 
 class index extends Component {
-constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-        data: {},
-        page: 1,
-        modal: false,
-        modalAction: 'delete',
-        modelToUse: '',
-        isDataLoaded: false,
-        hasError: false
+        this.state = {
+            data: {},
+            page: 1,
+            modal: false,
+            modalAction: 'delete',
+            modelToUse: '',
+            isDataLoaded: false,
+            hasError: false
     }
 
     const {t} = this.props;
@@ -51,130 +51,131 @@ constructor(props) {
     this.actionCloseModal = this.actionCloseModal.bind(this);
 }
 
-componentDidMount() {
-    FetchIndexData(this.props.api,'<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>', this.state.page, (response) => this.onDataFetched(response));
-}
+    componentDidMount() {
+        FetchIndexData(this.props.api,'<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>', this.state.page, (response) => this.onDataFetched(response));
+    }
 
-onDataFetched(response) {
-    let this_el = this;
-    let event = response.data;
-    return {
-        onSuccess: () => {
-            this_el.setState({data: event.data});
-            this_el.setState({isDataLoaded: true});
+    onDataFetched(response) {
+        let this_el = this;
+        let event = response.data;
+        return {
+            onSuccess: () => {
+                this_el.setState({data: event.data});
+                this_el.setState({isDataLoaded: true});
+            },
+            onError: () => {
+                console.log(response);
+                this_el.setState({ hasError: true}) ;
+            }
+        }
+    }
+
+    actionPagination(id) {
+        this.setState({page: id});
+        this.setState({isDataLoaded: false});
+        FetchIndexData(this.props.api,'<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>', id, (response) => this.onDataFetched(response));
+    }
+
+    actionCloseModal() {
+        this.setState({modal: false});
+        this.setState({modelToUse: ''});
+    }
+
+    actionOpenModal(id, type) {
+        this.setState({modelToUse: id});
+        this.setState({modal: true});
+        this.setState({modalAction: type});
+    }
+
+    onManipulateIndexData(response) {
+        let this_el = this;
+        return {
+            onSuccess: () => {
+                FetchIndexData(this_el.props.api,'<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>', this_el.state.page, (response) => this_el.onDataFetched(response));
+            this_el.actionCloseModal();
         },
-        onError: () => {
-            console.log(response);
-            this_el.setState({ hasError: true}) ;
+            onError: () => {
+                console.log(response);
+                this_el.setState({ hasError: true}) ;
+            }
         }
     }
-}
 
-actionPagination(id) {
-    this.setState({page: id});
-    this.setState({isDataLoaded: false});
-    FetchIndexData(this.props.api,'<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>', id, (response) => this.onDataFetched(response));
-}
-
-actionCloseModal() {
-    this.setState({modal: false});
-    this.setState({modelToUse: ''});
-}
-
-actionOpenModal(id, type) {
-    this.setState({modelToUse: id});
-    this.setState({modal: true});
-    this.setState({modalAction: type});
-}
-
-onManipulateIndexData(response) {
-    let this_el = this;
-    return {
-        onSuccess: () => {
-            FetchIndexData(this_el.props.api,'<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>', this_el.state.page, (response) => this_el.onDataFetched(response));
-        this_el.actionCloseModal();
-    },
-        onError: () => {
-            console.log(response);
-            this_el.setState({ hasError: true}) ;
+    modalCallbacks() {
+        return {
+            onClose: this.actionCloseModal,
+            onDelete: () => ManipulateIndexData(this.props.api, '<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>', 'delete', this.state.modelToUse, (response) => this.onManipulateIndexData(response)),
+            onRestore: () => ManipulateIndexData(this.props.api, '<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>', 'restore', this.state.modelToUse, (response) => this.onManipulateIndexData(response)),
         }
     }
-}
 
-modalCallbacks() {
-    return {
-        onClose: this.actionCloseModal,
-        onDelete: () => ManipulateIndexData(this.props.api, '<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>', 'delete', this.state.modelToUse, (response) => this.onManipulateIndexData(response)),
-        onRestore: () => ManipulateIndexData(this.props.api, '<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>', 'restore', this.state.modelToUse, (response) => this.onManipulateIndexData(response)),
-    }
-}
+    render() {
+        if (this.state.redirect) {
+            return <Redirect to={"/<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>"}/>;
+        }
 
-render() {
-    if (this.state.redirect) {
-        return <Redirect to={"/<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>"}/>;
-    }
+        //change this function to 'const {t} = this.props' when you have i18n translation enabled
+        const t = (data) => {return data}
 
-    //change this function to 'const {t} = this.props' when you have i18n translation enabled
-    const t = (data) => {return data}
-
-    return (
-        <div>
-            {PrepareIndexModal(this.modalCallbacks(), this.state.modal, this.state.modalAction)}
-            <div className={'index-page-header'}>
-                <h1><?= Inflector::camel2words(StringHelper::basename($generator->modelClass)) ?></h1>
-                <div className={'model-actions'}>
-                    <Link className="btn btn-success" to={'/<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>/create'}>Create</Link>
+        return (
+            <div>
+                {PrepareIndexModal(this.modalCallbacks(), this.state.modal, this.state.modalAction)}
+                <div className={'index-page-header'}>
+                    <h1><?= Inflector::camel2words(StringHelper::basename($generator->modelClass)) ?></h1>
+                    <div className={'model-actions'}>
+                        <Link className="btn btn-success" to={'/<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>/create'}>Create</Link>
+                    </div>
                 </div>
-            </div>
 
-            {
-                (this.state.isDataLoaded) ?
-                <DataView
-                    models={this.state.data.data}
-                    modelName={"<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>"}
-                    className={""}
-                    attributes={{
-                        <?php
-                            $count = 0;
-                            if (($tableSchema = $generator->getTableSchema()) === false) {
-                                foreach ($generator->getColumnNames() as $name) {
-                                    if (++$count < 6) {
-                                        echo $name .":"."'" . $name . "',\n";
-                                    } else {
-                                        echo "//'" . $name .":"."'" . $name . "',\n";
+                {
+                    (this.state.isDataLoaded) ?
+                    <DataView
+                        models={this.state.data.data}
+                        modelName={"<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>"}
+                        className={""}
+                        attributes={{
+                            <?php
+                                $count = 0;
+                                if (($tableSchema = $generator->getTableSchema()) === false) {
+                                    foreach ($generator->getColumnNames() as $name) {
+                                        if (++$count < 6) {
+                                            echo $name .":"."'" . $name . "',\n";
+                                        } else {
+                                            echo "//'" . $name .":"."'" . $name . "',\n";
+                                        }
+                                    }
+                                } else {
+                                    foreach ($tableSchema->columns as $column) {
+                                        $format = $generator->generateColumnFormat($column);
+                                        if (++$count < 6) {
+                                            echo $column->name .":"."'" . $column->name . "',\n";
+                                        } else {
+                                            echo "//'" . $column->name .":"."'" . $column->name . "',\n";
+                                        }
                                     }
                                 }
-                            } else {
-                                foreach ($tableSchema->columns as $column) {
-                                    $format = $generator->generateColumnFormat($column);
-                                    if (++$count < 6) {
-                                        echo $column->name .":"."'" . $column->name . "',\n";
-                                    } else {
-                                        echo "//'" . $column->name .":"."'" . $column->name . "',\n";
-                                    }
-                                }
-                            }
-                        ?>
-                    }}
-                    actions={{
-                        update: {title: "update"},
-                        delete: {title: "delete"},
-                        restore: {title: "restore"},
-                    }}
-                    modalActions={{
-                        onModalOpen: this.actionOpenModal,
-                    }}
-                    ifEmpty={"Create first element"}
-                    pagination={{
-                        currentPage: this.state.page,
-                        totalPages: this.state.data.total_pages,
-                        callback: this.actionPagination
-                    }}
-                />
-                : <IndexDataLoader />
-            </div>
-        );
-    }
+                            ?>
+                        }}
+                        actions={{
+                            update: {title: "update"},
+                            delete: {title: "delete"},
+                            restore: {title: "restore"},
+                        }}
+                        modalActions={{
+                            onModalOpen: this.actionOpenModal,
+                        }}
+                        ifEmpty={"Create first element"}
+                        pagination={{
+                            currentPage: this.state.page,
+                            totalPages: this.state.data.total_pages,
+                            callback: this.actionPagination
+                        }}
+                    />
+                    : <IndexDataLoader />
+                }
+                </div>
+            );
+        }
 }
 
 const mapStateToProps = state => ({
